3fc932b84dd5d496cf582e99914ef0e964daa25f
smackagency <selvinkuik@gmail.com>
2022-05-06 14:43:23 +0100

    Added simply-static-1-1651844567

---
+++ wp-content/themes/site/static/js/app.ca94571d0f1f58d4df92.js
@@ -0,0 +1,1 @@
+!function(){var e=document.querySelector(".burger"),c=document.querySelector(".header__nav"),t=!1;e&&e.addEventListener("click",(function(){t?(e.classList.remove("--active"),c.classList.remove("--active"),t=!1):(e.classList.add("--active"),c.classList.add("--active"),t=!0)}))}();
